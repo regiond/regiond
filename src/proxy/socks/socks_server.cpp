d01abbd52f8bebe233fdfcd65a9d9cb905ba2cd2
Johannes Ekberg <uppfinnarn@gmail.com>
2014-11-07 07:38:10 +0100

    [namespacing intensifies]

--- src/proxy/socks/socks_server.cpp
+++ src/proxy/socks/socks_server.cpp
@@ -1,8 +1,9 @@
 #include "socks_server.h"
 
 using namespace regiond;
-using namespace regiond::socks;
+using namespace regiond::proxy::socks;
 using namespace boost::asio;
+using namespace boost::asio::ip;
 
 socks_server::socks_server(io_service &service):
 	service(service)
@@ -15,7 +16,7 @@ socks_server::~socks_server()
 	
 }
 
-bool socks_server::bind(ip::tcp::endpoint ep)
+bool socks_server::bind(tcp::endpoint ep)
 {
 	return false;
 }
